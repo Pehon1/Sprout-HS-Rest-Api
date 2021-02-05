92f91944b660fcbaf663c132a93d0154b270afd4
PeHon <pehon@ong.ph>
2021-02-05 14:21:23 +0800

    catch errors

--- server.js
+++ server.js
@@ -15,8 +15,13 @@ const hubspot = require('@hubspot/api-client')
 const hubspotClient = new hubspot.Client({ apiKey: process.env.HUBSPOT_KEY })
 
 async function queryHubspotForUserEmailWithUserId(userId) {
-    const email = await hubspotClient.crm.contacts.basicApi.getById(userId)
-    return email.body.properties.email
+    try {
+        const email = await hubspotClient.crm.contacts.basicApi.getById(userId)
+        return email.body.properties.email
+    } catch {
+        return ""
+    }
+    
 }
 
 async function queryHubspotForUserWithConversationId(taskId) {
@@ -24,16 +29,27 @@ async function queryHubspotForUserWithConversationId(taskId) {
     if (contact.body.errors == undefined && contact.body.status == 'COMPLETE') {
         if (contact.body.results.length >= 1 && contact.body.results[0].to.length >= 1) {
             var contact_id = contact.body.results[0].to[0].id
-            var email = await queryHubspotForUserEmailWithUserId(contact_id)
-            return email
+            try {
+                //var email = await queryHubspotForUserEmailWithUserId("12434213")
+                var email = await queryHubspotForUserEmailWithUserId(contact_id)
+                return email
+            } catch {
+                return ""
+            }
         }
+    } else {
+        return ""
     }
 }
 
 app.route('/conversations/:conversationId')
     .get(async function (req, res) {
-        var email = await queryHubspotForUserWithConversationId(req.params.conversationId)
-        res.jsonp(email)
+        try {
+            var email = await queryHubspotForUserWithConversationId(req.params.conversationId)
+            res.jsonp(email)
+        } catch {
+            res.jsonp("")
+        }
     });
 
 app.listen(port);
