78b36cc63d58188555ddd9c3636c8293af51d46a
PeHon <pehon@ong.ph>
2021-02-05 10:42:10 +0800

    output fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const hubspotClient = new hubspot.Client({ apiKey: process.env.HUBSPOT_KEY })
 
 async function queryHubspotForUserEmailWithUserId(userId) {
     const email = await hubspotClient.crm.contacts.basicApi.getById(userId)
-    return email.body.properties.email
+    return { email: email.body.properties.email }
 }
 
 async function queryHubspotForUserWithConversationId(taskId) {
