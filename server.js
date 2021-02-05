091b5aebdc3ce6d4b8c170885a2a8f2938f3c8f0
PeHon <pehon@ong.ph>
2021-02-05 10:04:14 +0800

    CORS enabled

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 
+
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
